89bb6265ba92e824a5aa3638ea2309f0ca9e79f2
Art Frich <frich.g23@yandex.ru>
2023-06-26 21:55:01 +0500

    feat: prepair header and api-query

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,15 +1,21 @@
 import PageWithForm from "./PageWithForm";
 import { Link } from "react-router-dom";
+import InfoTooltip from "./popups/InfoTooltip";
 
 export default function Register(){
+  function handleSubmit( e ){
+    e.preventDefault();
+  }
+
   return(
     <PageWithForm 
       textTitle="Регистрация" 
       textBtnSbt="Зарегистрироваться" 
       textLabelBtn="Уже зарегистрированы? Войти"
+      onSubmit={ handleSubmit }
     >
       <p className="page-auth__label-btn">
-        Уже зарегистрированы? <Link className="page-auth__label-btn-link transition-opacity" to='/sign-up'>Войти</Link>
+        Уже зарегистрированы? <Link className="page-auth__label-btn-link transition-opacity" to='/sign-in'>Войти</Link>
       </p>
     </PageWithForm>
   )
