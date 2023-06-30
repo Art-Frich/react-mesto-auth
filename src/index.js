94a487bc265813bd5453003ef1c53cd881c5f549
Art Frich <frich.g23@yandex.ru>
2023-06-30 16:37:47 +0500

    fix: gh-pages fix

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import './pages/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App.js';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-mesto-auth">
+    <HashRouter basename="/react-mesto-auth">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
